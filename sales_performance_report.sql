56203ced361338e25da7764cf09e64a1b5d605c7
Venthy Hanna <93747394+veyhanna@users.noreply.github.com>
2022-03-21 10:25:35 +0800

    Update sales_performance_report.sql

--- sales_performance_report.sql
+++ sales_performance_report.sql
@@ -1,14 +1,14 @@
 #Overall Performance by Year
 select year(order_date) as years, 
-		sum(sales) as sales, 
-		count(order_quantity) as number_of_order	
+	sum(sales) as sales, 
+	count(order_quantity) as number_of_order	
 from dqlab_sales_store
 where order_status = 'order finished'
 group by year(order_date);
 
 #Overall Performance by Product Sub Category
 select year(order_date) as years, 
-		product_sub_category, 
+	product_sub_category, 
         sum(sales) as sales
 from dqlab_sales_store
 where order_status = 'order finished' and year(order_date) in ('2011', '2012')
@@ -17,7 +17,7 @@ order by years, sum(sales) desc;
 
 #Promotion Effectiveness and Efficiency by Years
 select year(order_date) as years, 
-		sum(sales) as sales, 
+	sum(sales) as sales, 
         sum(discount_value) as promotion_value, 
         round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
 from dqlab_sales_store
@@ -26,10 +26,10 @@ group by years;
 
 #Promotion Effectiveness and Efficiency by Product Sub Category
 select year(order_date) as years, 
-		product_sub_category, 
-		product_category, sum(sales) as sales, 
-		sum(discount_value) as promotion_value, 
-		round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
+	product_sub_category, 
+	product_category, sum(sales) as sales, 
+	sum(discount_value) as promotion_value, 
+	round(sum(discount_value)/sum(sales)*100,2) as burn_rate_percentage
 from dqlab_sales_store
 where order_status = 'order finished' and year(order_date) = '2012'
 group by years, product_sub_category, product_category
@@ -37,7 +37,7 @@ order by sales desc;
 
 #Customers Transactions per Year
 select year(order_date) as years,
-		count(distinct customer) as number_of_customer
+	count(distinct customer) as number_of_customer
 from dqlab_sales_store
 where order_status = 'order finished'
 group by years;
